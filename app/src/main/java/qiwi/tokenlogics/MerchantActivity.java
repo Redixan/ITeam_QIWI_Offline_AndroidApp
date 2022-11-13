8a41f80b4980c84f6aa33a79e3abb82c88f4565e
Alex <synarge2027@gmail.com>
2022-11-13 06:40:25 +0300

    add Scanner for merchant

--- app/src/main/java/qiwi/tokenlogics/MerchantActivity.java
+++ app/src/main/java/qiwi/tokenlogics/MerchantActivity.java
@@ -1,6 +1,7 @@
 package qiwi.tokenlogics;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,6 +10,10 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MerchantActivity extends AppCompatActivity {
 
@@ -38,7 +43,26 @@ public class MerchantActivity extends AppCompatActivity {
     }
 
     public void onClickPayByQR(View view) {
-        Intent intent = new Intent(this, QRpayPurchaserAcitivity.class);
-        startActivity(intent);
+        IntentIntegrator intentIntegrator = new IntentIntegrator(this);
+        intentIntegrator.setPrompt("Scan a barcode or QR Code");
+        intentIntegrator.setOrientationLocked(true);
+        intentIntegrator.setOrientationLocked(false);
+        intentIntegrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (intentResult != null) {
+            if (intentResult.getContents() == null) {
+                Toast.makeText(getBaseContext(), "Scanner cancelled", Toast.LENGTH_SHORT).show();
+            } else {
+                //messageLabel.setText(intentResult.getContents());
+                // LOGIC HERE after scanning
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
