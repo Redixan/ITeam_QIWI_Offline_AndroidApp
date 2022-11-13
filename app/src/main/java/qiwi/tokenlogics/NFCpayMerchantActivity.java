7d4e0a655556f2e31ea7b5fb7df6e21fbdaa1e6a
Alex <synarge2027@gmail.com>
2022-11-13 10:31:34 +0300

    add generate QR for client

--- app/src/main/java/qiwi/tokenlogics/NFCpayMerchantActivity.java
+++ app/src/main/java/qiwi/tokenlogics/NFCpayMerchantActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class NFCpayMerchantActivity extends AppCompatActivity {
+public class   NFCpayMerchantActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
